9a05ea3b182b1c302b986082bd92135449656708
Justin Thomas <thomasjustin2203@gmail.com>
2024-02-23 15:00:20 +0530

    last commit

--- lib/controllers/checkbox_provider.dart
+++ lib/controllers/checkbox_provider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+/* This class handles the logic of custom dropdown button.
+Actually the state handling of the  dropdown class is handled by a provider class */
 class CheckboxProvider extends ChangeNotifier {
  bool _isCheckedTerms = false;
   bool _isCheckedMarketing = false;
@@ -7,11 +8,13 @@ class CheckboxProvider extends ChangeNotifier {
   bool get isCheckedTerms => _isCheckedTerms;
   bool get isCheckedMarketing => _isCheckedMarketing;
 
+//Terms& condition checkbox state handling
   void toggleTerms(bool newValue) {
     _isCheckedTerms = newValue;
     notifyListeners();
   }
 
+//Marketing checkbox state handling
   void toggleMarketing(bool newValue) {
     _isCheckedMarketing = newValue;
     notifyListeners();
--- lib/controllers/dropdown_provider.dart
+++ lib/controllers/dropdown_provider.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
-
+/*This class handles when ever we click the dropdown button the dropdown list is appearing in the UI through the
+help of this provier class  
+*/
 class DropdownProvider extends ChangeNotifier {
   String? _selectedLanguage;
 
   String? get selectedLanguage => _selectedLanguage;
 
+//Setter method for changing the value
   void setSelectedLanguage(String language) {
     _selectedLanguage = language;
     notifyListeners();
@@ -13,6 +16,7 @@ class DropdownProvider extends ChangeNotifier {
 
   bool get isVisible => _isVisible;
 
+//Setter method for dropdown visibility
   void toggleVisibility() {
     _isVisible = !_isVisible;
     notifyListeners();
